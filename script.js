dd82e29a69736e1bc2698b328e61b1faf9889d13
Paul Han <paul.myung.han@gmail.com>
2020-02-06 23:19:44 -0800

    psuedocoded entire hw, debugged color changing function and can't figure out how to get individuals row to change and not entire table

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 //create html variables here
-
 let schedule = $(".schedule");
-let agenda;
 
+//function that gets user input and saves to local storage
 
-//function that prompts user to put in schedule for the hour
-
-
-
-
+//specific function to grab schedule section after clicking save button
+//having trouble get the button to reference sibling tag
 $(".save").on("click", function(e){
     e.preventDefault();
-    console.log($(this).siblings('.schedule'));
-    localStorage.setItem(hour, agenda);
+    console.log($(this).parents('.schedule'));
+    //localStorage.setItem(...)
    
 })
 
+
+
+//function to get all saved items from local storage and display it on the schedule section
 function getAllHours(){
     $("#text1").val(localStorage.getItem("hour1"));
     localStorage.getItem("hour2")
@@ -25,41 +24,39 @@ function getAllHours(){
     localStorage.getItem("hour1")
     localStorage.getItem("hour1")
 }
-//function that saves ones agenda to the local storage so it stays even upon refreshing the page
-// saveButton.on("click", saveAgenda);
 
 
-// let hour = 1;
-// function saveAgenda(){
-//     let hour =1;
-//     
-//     schedule.text(`${localStorage.getItem(agenda)}`)
 
-// }
+
+
+// function that changes color of schedule text box based on what hour it is
+
 
 let hour = $(".hour")
-//function that changes color of schedule text box based on what hour it is
+
 function checkTime(){
    hour.each(function(){
     //    let currentTime = moment().format('HH');
        let currentTime = 12
-       let plannerTime = $(this).children().attr("id")
+       let plannerTime = parseInt($(this).children().attr("id"))
        console.log(currentTime);
        console.log(plannerTime);
-
-    //    console.log($(this).children().attr("id"));
-    //    console.log(moment().format('HH'))
+       debugger;
        if(currentTime < plannerTime){
         schedule.css("background-color", "#77dd77");
+        $("input").css("background-color", "#77dd77" )
        }
        else if(currentTime === plannerTime){
         schedule.css("background-color", "#ff6961");
+        $("input").css("background-color", "#ff6961");
        }
 
        else{
         schedule.css("background-color", "grey");
+        $("input").css("background-color", "grey");
        }
-   })
+   });
+
 }
 
 checkTime();
