41db785830b5e403600f7db023926a3283e94aba
Paul Han <paul.myung.han@gmail.com>
2020-02-06 18:51:38 -0800

    started all three functions

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 //create html variables here
-let saveButton = $(".save")
+
+let schedule = $(".schedule");
+let agenda;
 
 
-saveButton.on("click", saveAgenda);
+//function that prompts user to put in schedule for the hour
 
-function saveAgenda(){
-    
 
+
+
+$(".save").on("click", function(e){
+    e.preventDefault();
+    console.log($(this).siblings('.schedule'));
+    localStorage.setItem(hour, agenda);
+   
+})
+
+function getAllHours(){
+    $("#text1").val(localStorage.getItem("hour1"));
+    localStorage.getItem("hour2")
+    localStorage.getItem("hour1")
+    localStorage.getItem("hour1")
+    localStorage.getItem("hour1")
+}
+//function that saves ones agenda to the local storage so it stays even upon refreshing the page
+// saveButton.on("click", saveAgenda);
+
+
+// let hour = 1;
+// function saveAgenda(){
+//     let hour =1;
+//     
+//     schedule.text(`${localStorage.getItem(agenda)}`)
+
+// }
+
+let hour = $(".hour")
+//function that changes color of schedule text box based on what hour it is
+function checkTime(){
+   hour.each(function(){
+       console.log($(this).children().attr("id"));
+   })
 }
 
+checkTime();
+
+//todo list:
+// 1) make the agenda boxes text boxes
+// 2) create save function that gets the agenda box and saves to local storage
+// 3) create function that colors the agenda boxes red for current hour, green for future hours, or current grey for past
