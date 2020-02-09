a06d072893ebfc41c87b0ee50e6e5123dfbfceaf
Paul Han <paul.myung.han@gmail.com>
2020-02-08 18:23:11 -0800

    changed color scheme, and took out unneccesary code such as comments

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let schedule = $(".schedule");
 $(".save").on("click", function(e){
     e.preventDefault();
     let agenda = $(this).parent().siblings('.schedule').find("input").val();
-    // let agenda2 = $(this).parent().siblings('.schedule').find("input")
     hour = parseInt($(this).attr("id"));
     localStorage.setItem(hour, agenda); 
     console.log(localStorage.getItem(hour))
@@ -33,26 +32,17 @@ let hour = $(".hour")
 
 function checkTime(){
    hour.each(function(){
-       let currentInput = $(this).children().attr("id")
-    //    let currentTime = moment().format('HH');
-       let currentTime = 12
-       let plannerTime = parseInt($(this).children().attr("id"))
-    //    console.log(currentTime);
-    //    console.log(plannerTime);
-       debugger;
-       if(currentTime < plannerTime){
-        console.log(currentTime, plannerTime);
-        schedule.css("background-color", "#77dd77");
-        $(`#${plannerTime}`).css("background-color", "#77dd77" )
-       }
-       else if(currentTime === plannerTime){
-        schedule.css("background-color", "#ff6961");
-        $(`#${plannerTime}`).css("background-color", "#ff6961")
-       }
+       let currentTime = moment().format('HH');     
+       let plannerTime = parseInt($(this).children().attr("id"));
 
+       if(currentTime < plannerTime){        
+        $(`#${plannerTime}`).css("background-color", "#77dd77" );
+       }
+       else if(currentTime === plannerTime){ 
+        $(`#${plannerTime}`).css("background-color", "#ff6961");
+       }
        else{
-        schedule.css("background-color", "grey");
-        $(`#${plannerTime}`).css("background-color", "grey" )
+        $(`#${plannerTime}`).css("background-color", "grey" );
        }
    });
 
